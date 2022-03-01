e8ed7f5c6911d8307ed30f12ce08d12ffc28adbe
AmanHussain2929 <77919593+AmanHussain2929@users.noreply.github.com>
2022-03-01 20:51:11 +0530

    Added solution - LeetHub

---
+++ Multiply two linked lists - GFG/multiply-two-linked-lists.java
@@ -0,0 +1,98 @@
+// { Driver Code Starts
+import java.util.*;
+class Node
+{
+    int data;
+    Node next;
+    Node(int data){
+        this.data=data;
+        next=null;
+    }
+}
+
+class MultiplyTwoLL{
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0){
+			Node head=null;
+			Node phead=null;
+			int n=sc.nextInt();
+			while(n-->0){
+				int n1=sc.nextInt();
+				if(head==null)
+				head=new Node(n1);
+			else{
+				addHead(head,n1);
+			}
+			}
+			int m=sc.nextInt();
+			while(m-->0){
+				int n1=sc.nextInt();
+				if(phead==null)
+				phead=new Node(n1);
+			else{
+				addPhead(phead,n1);
+			}
+			}
+		GfG g=new GfG();
+
+System.out.println(g.multiplyTwoLists(head,phead));
+		}
+	}
+    public static void addHead(Node node,int a) {
+          if (node == null)
+            return;
+		if(node.next==null)
+			node.next=new Node(a);
+		else
+			addHead(node.next,a);
+	}
+	public static void addPhead(Node node,int a){
+		 if (node == null)
+            return;
+		if(node.next==null)
+		node.next=new Node(a);
+	else
+		addPhead(node.next,a);
+	}
+	}
+	
+	// } Driver Code Ends
+
+
+/*Node is defined as
+class Node
+{
+    int data; 
+    Node next;
+    Node(int data) {
+        this.data=data;
+        this.next = null;
+    }
+}*/
+
+class GfG{
+  /*You are required to complete this method */
+   public long multiplyTwoLists(Node l1,Node l2){
+       String g1="",g2="";
+       while(l1!=null)
+       {
+          g1=g1+String.valueOf(l1.data);
+          l1=l1.next;
+       }
+        while(l2!=null)
+       {
+            g2=g2+String.valueOf(l2.data);
+          l2=l2.next;
+       }
+       long ans;
+    java.math.BigInteger m1 = new java.math.BigInteger(g1);
+   java.math.BigInteger m2 = new java.math.BigInteger(g2);
+   java.math.BigInteger product = m1.multiply(m2);
+   java.math.BigInteger m = new java.math.BigInteger("1000000007");
+   java.math.BigInteger p = product.mod(m);
+   ans= p.longValue();
+   return ans;
+   }
+} 
