61431e60891a641b69d2f2729dc6097d6419621b
AmanHussain2929 <77919593+AmanHussain2929@users.noreply.github.com>
2022-11-01 14:34:03 +0530

    Time: 218 ms (5.92%), Space: 20.1 MB (92.01%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.cpp
+++ 2418-sort-the-people/2418-sort-the-people.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
     vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
-       int m=heights.size()-2;
-        while(m>=0)
-        {
-            int e=0;
-            int f=1;
-            while(e<=m)
-            {
-                if(heights[e]<heights[f])
-                {
-                    swap(heights[e],heights[f]);
-                    swap(names[e],names[f]);
-                }
-                e++;
-                f++;
-            }
-            m--;
-        }
+        int n=heights.size();
+    int y=1;
+        string s;
+  while(y<=n-1)
+  {
+        int num=heights[y];
+           s=names[y];
+       int j=y-1;
+     while(j>=0 && heights[j]<num)
+     {
+              heights[j+1]=heights[j];
+              names[j+1]=names[j];
+          j--;
+     }
+      heights[j+1]=num;
+      names[j+1]=s;
+      y++;
+  }
         return names;
+    
     }
 };
